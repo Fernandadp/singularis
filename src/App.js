9166566d5ae44888b4e374334f27bb561a569d5c
Fernanda Dehays <fernanda.dehays@fernandas-mbp.localdomain>
2020-05-28 21:03:02 +0200

    start to configure react router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import React from 'react'
+import { Route, NavLink } from 'react-router-dom'
 
-import './App.css';
 import Home from './components/Home'
 import Servicios from './components/Servicios'
 import Centro from './components/Centro'
 import Team from './components/Team'
 import Contacto from './components/Contacto'
+import './App.css';
 
 function App() {
   return (
     <>
-      <Home />
-      <Servicios />
-      <Centro />
-      <Team />
-      <Contacto />
+      <h1>SINGULARIS</h1>
+      <ul className="header">
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/servicios">Servicios</NavLink></li>
+        <li><NavLink to="/centro">Centro</NavLink></li>
+        <li><NavLink to="/team">Team</NavLink></li>
+        <li><NavLink to="/contacto">Contacto</NavLink></li>
+      </ul>
+
+      <div className="content">
+        <Route path="/" component={Home} />
+        <Route path="/servicios" component={Servicios} />
+        <Route path="/centro" component={Centro} />
+        <Route path="/team" component={Team} />
+        <Route path="/contacto" component={Contacto} />
+      </div>
     </>
   );
 }
