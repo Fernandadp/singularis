c9b2b14e338a47b858b1aade8e41a973bbd6a7cb
Fernanda Dehays <fernanda.dehays@fernandas-mbp.localdomain>
2020-05-29 20:08:02 +0200

    add room pictures, some style for navbar

--- src/App.js
+++ src/App.js
@@ -6,22 +6,40 @@ import Servicios from './components/Servicios'
 import Centro from './components/Centro'
 import Team from './components/Team'
 import Contacto from './components/Contacto'
+import Logo from './img/singularis_logo_small.jpg'
 import './App.css';
 
 function App() {
   return (
     <>
-      <h1>SINGULARIS</h1>
-      <ul className="header">
-        <li><NavLink to="/">Home</NavLink></li>
-        <li><NavLink to="/servicios">Servicios</NavLink></li>
-        <li><NavLink to="/centro">Centro</NavLink></li>
-        <li><NavLink to="/team">Team</NavLink></li>
-        <li><NavLink to="/contacto">Contacto</NavLink></li>
-      </ul>
+      <div className="header"> 
+        <img src={Logo} alt="logo singularis" />
+        <ul className="navbar">
+          <li className="navbar-link"><NavLink to="/">Home</NavLink></li>
+          <li className="navbar-link"><NavLink to="/servicios">Servicios</NavLink></li>
+          <li className="navbar-link"><NavLink to="/centro">Centro</NavLink></li>
+          <li className="navbar-link"><NavLink to="/team">Team</NavLink></li>
+          <li className="navbar-link"><NavLink to="/contacto">Contacto</NavLink></li>
+          <li className="hamburguerMenu" id="mobileIcon">
+            <div className="linea" id="linea-1"></div>
+            <div className="linea" id="linea-2"></div>
+            <div className="linea" id="linea-3"></div>
+          </li>
+        </ul>
+
+        <div className="mobile-menu">
+          <ul id="mobileMenu">
+            <li><NavLink to="/" className="mobileLink">Home</NavLink></li>
+            <li><NavLink to="/servicios" className="mobileLink">Servicios</NavLink></li>
+            <li><NavLink to="/centro" className="mobileLink">Centro</NavLink></li>
+            <li><NavLink to="/team" className="mobileLink">Team</NavLink></li>
+            <li><NavLink to="/contacto" className="mobileLink">Contacto</NavLink></li>
+          </ul>
+        </div>
+      </div>
 
       <div className="content">
-        <Route path="/" component={Home} />
+        <Route exact path="/" component={Home} />
         <Route path="/servicios" component={Servicios} />
         <Route path="/centro" component={Centro} />
         <Route path="/team" component={Team} />
