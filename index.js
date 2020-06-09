596dafea2499887a666dfa77155c7a159222ae43
Fernanda Dehays <fernanda.dehays@fernandas-mbp.localdomain>
2020-06-09 21:34:50 +0200

    add simple slider, make font bigger

--- index.js
+++ index.js
@@ -11,33 +11,17 @@ function showMobileMenu(id) {
   }
 }
 
-//var slideIndex = 0;
-//showSlides();
-//
-//function showSlides() {
-//  var i;
-//  var slides = document.getElementsByClassName("mySlides");
-//  for (i = 0; i < slides.length; i++) {
-//    slides[i].style.display = "none";
-//  }
-//  slideIndex++;
-//  if (slideIndex > slides.length) {slideIndex = 1}
-//  slides[slideIndex-1].style.display = "block";
-//  setTimeout(showSlides, 2000); // Change image every 2 seconds
-//}
-//
-//$(document).ready(function(){
-//  $('.slider').slick({
-//    setting-name: setting-value
-//  });
-//});
-//
-//
-//$('.one-time').slick({
-//  dots: true,
-//  infinite: true,
-//  speed: 300,
-//  slidesToShow: 1,
-//  adaptiveHeight: true
-//});
-		
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
